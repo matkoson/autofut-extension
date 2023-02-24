43ba2c6169c19a733fb84cc8a62304588ad99986
Mateusz Koson <mat@koson.in>
2023-02-24 12:50:01 +0100

    chore: cleanup

--- extension/page_scripts/page_script.d_162110.ts
+++
@@ -1,4 +0,0 @@
-declare const extensionName: "autofut";
-declare function simulateKeyEvent(eventType: any, el: any, args: any): void;
-declare const jsonEl: HTMLDivElement;
-//# sourceMappingURL=page_script.d.ts.map
